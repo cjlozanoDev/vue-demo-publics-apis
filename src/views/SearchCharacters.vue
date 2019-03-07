d5ccecfe7b7e4592a881d1902b7c649634930948
cjlozano <carlosjlp@gmail.com>
2019-03-07 16:53:20 +0100

    views creadas

---
+++ src/views/SearchCharacters.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <v-btn color="primary">Success</v-btn>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Search Character',
+  data () {
+    return {
+      msg: 'Buscador de personajes Marvel'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
